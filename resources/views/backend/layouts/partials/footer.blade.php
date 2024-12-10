5c5ea4da14b3933ea2703bd2e2a895416a35026a
Nur Mohammad Akram <nur.akram@nccbank.com.bd>
2024-12-10 15:36:33 +0600

    initial

---
+++ resources/views/backend/layouts/partials/footer.blade.php
@@ -0,0 +1,8 @@
+
+<!-- footer area start-->
+<footer>
+    <div class="footer-area">
+        <p>© Copyright 2024. All right reserved. NCC DFS <a href="https://colorlib.com/wp/"></a>.</p>
+    </div>
+</footer>
+<!-- footer area end-->
